66cd1a31f4a21a3832b58fbcd5e45e828c0b33fd
ansuvin <50893537+ansuvin@users.noreply.github.com>
2020-06-30 17:22:19 +0900

    20200630

---
+++ Ex07_Class/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex07_Class
+{
+    class Cube
+    {
+        static int countOfInstance;
+
+        int x, y, z;
+
+        public void GetVolume()
+        {
+
+            Console.WriteLine("생성된 Cube의 개수" + countOfInstance);
+            Console.WriteLine("Cube의 부피" + x * y * z);
+        }
+
+        public Cube(int x, int y, int z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+            countOfInstance++;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cube cube1 = new Cube(2, 3, 4);
+            cube1.GetVolume();
+
+            Cube cube2 = new Cube(5, 5, 5);
+            cube2.GetVolume();
+        }
+    }
+}
+
